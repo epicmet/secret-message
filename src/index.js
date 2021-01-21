0b1279940dc7d9e98e38ad07d7ce6135e18f7c3a
mahdi aghaei <mhd.agi01@gmail.com>
2021-01-21 22:06:12 +0330

    basic encryption and html stuff

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 document.querySelector('form').addEventListener('submit', (event) => {
     event.preventDefault();
 
-    const input = document.querySelector('input');
-    console.log(input.value);
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#link-form').classList.remove('hide');
+    
+    const input = document.querySelector('#message-input');
+    const encrypted = btoa(input.value);
+
+    const linkInput = document.querySelector('#link-input');
+    linkInput.value = `${window.location}#${encrypted}`;
+    linkInput.select();
 });
