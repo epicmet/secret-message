bb15d4de95c5fc895f80080d3b887796ac86699c
mahdi aghaei <mhd.agi01@gmail.com>
2021-01-21 22:41:51 +0330

    complete

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ document.querySelector('form').addEventListener('submit', (event) => {
 
     document.querySelector('#message-form').classList.add('hide');
     document.querySelector('#link-form').classList.remove('hide');
-    
+
     const input = document.querySelector('#message-input');
     const encrypted = btoa(input.value);
 
@@ -11,3 +11,12 @@ document.querySelector('form').addEventListener('submit', (event) => {
     linkInput.value = `${window.location}#${encrypted}`;
     linkInput.select();
 });
+
+const { hash } = window.location;
+const message = atob(hash.replace('#', ''));
+if(message){
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#message-show').classList.remove('hide');
+
+    document.querySelector('h1').innerHTML = message;
+}
