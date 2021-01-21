0a2787669d2dc8df9a1892f66aa0ccef49aacbc2
mahdi aghaei <mhd.agi01@gmail.com>
2021-01-21 21:41:43 +0330

    added basic codes

---
+++ src/index.js
@@ -0,0 +1,6 @@
+document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const input = document.querySelector('input');
+    console.log(input.value);
+});
